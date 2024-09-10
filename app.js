e7fe74b2f9e073e7e5a69eae8bffaeb94cd5a51a
glVerdi <gabrielverdi12@gmail.com>
2024-09-10 08:09:36 -0300

    Melhorando o site

--- app.js
+++ app.js
@@ -1,19 +1,35 @@
 function pesquisar() {
-    // Obtém a seção onde os resultados serão exibidos
-    let section = document.getElementById("resultados-pesquisa");
-  
-    // Inicializa uma string vazia para armazenar os resultados
-    let resultados = "";
-  
-    // Itera sobre cada dado na lista de dados
-    for (let dado of dados) {
-      // Cria o HTML para cada resultado e adiciona à string 'resultados'
-      resultados += `
-      <div class="item-resultado">
-        <h2>${dado.titulo}</h2> <p class="descrição-meta">${dado.descricao}</p> <a href="${dado.link}" target="_blank">Mais informações</a> </div>
-    `;
-    }
-  
-    // Atribui a string de resultados ao conteúdo HTML da seção
-    section.innerHTML = resultados;
+  // Obtém a seção onde os resultados serão exibidos
+  let section = document.getElementById("resultados-pesquisa");
+
+  let campoPesquisa = document.getElementById("campo-pesquisa").value;
+
+  // se campoPesquisa for uma String sem nada
+  if (campoPesquisa == "") {
+    section.innerHTML = "<p>Nenhum investimento encontrado</P>"
+    return 
   }
+
+  campoPesquisa = campoPesquisa.toLowerCase();
+
+  // Inicializa uma string vazia para armazenar os resultados, titulo e a descição
+  let resultados = "";
+  let titulo = "";
+  let descricao = "";
+
+  // Itera sobre cada dado na lista de dados
+  for (let dado of dados) {
+    titulo = dado.titulo.toLowerCase()
+    descricao = dado.descricao.toLowerCase()
+    // se titulo includes campoPesquisa
+    if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa)) {
+      // então, Cria um novo elemento
+      resultados += `
+        <div class="item-resultado">
+          <h2>${dado.titulo}</h2> <p class="descrição-meta">${dado.descricao}</p> <a href="${dado.link}" target="_blank">Mais informações</a> </div>
+      `;
+    }
+  }
+  // Atribui a string de resultados ao conteúdo HTML da seção
+  section.innerHTML = resultados;
+}
